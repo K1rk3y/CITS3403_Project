a961092bb19d20d7778c0df4980800a070c380ca
Nishk139 <143082293+Nishk139@users.noreply.github.com>
2024-05-14 22:53:36 +0800

    Modified fields, added reference images, updated the frontend
    Joyoti's Backend on Create-Requests Page. Integrating front-end form validation and search function. And also adding the images and styles from the front-end.
    
    Co-Authored-By: Sreelajoyoti <87124856+Sreelajoyoti@users.noreply.github.com>

--- Proj/app/forms.py
+++ Proj/app/forms.py
@@ -1,13 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SelectField, SubmitField, IntegerField, TextAreaField
+from wtforms.validators import DataRequired, Email
+from app.validators import phone_number_validator, address_validator, instructions_length_validator, number_of_people_validator
 
 class OrderForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
-    address = StringField('Address', validators=[DataRequired()])
-    suburb = StringField('Suburb')
-    email = StringField('Email', validators=[DataRequired()])
-    phone = StringField('Phone')
-    meal = SelectField('Meal', choices=[('normal', 'Normal'), ('vegan', 'Vegan'), ('vegetarian', 'Vegetarian'), ('pescatarian', 'Pescatarian'), ('nut-free', 'Nut-Free')])
+    address = StringField('Address', validators=[DataRequired(), address_validator])
+    suburb = StringField('Suburb', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    phone = StringField('Phone', validators=[DataRequired(), phone_number_validator])
+    meal = SelectField('Meal', choices=[
+        ('normal', 'Normal'), ('vegan', 'Vegan'),
+        ('vegetarian', 'Vegetarian'), ('pescatarian', 'Pescatarian'),
+        ('nut-free', 'Nut-Free')
+    ], validators=[DataRequired()])
+    number_of_people = IntegerField('Number of People', validators=[DataRequired(), number_of_people_validator])
+    instructions = TextAreaField('Special Instructions', validators=[instructions_length_validator])
     submit = SubmitField('Submit')
+
+
+class SearchForm(FlaskForm):
+    query = StringField('Search Orders', validators=[DataRequired()])
+    submit = SubmitField('Search')
