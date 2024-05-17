647a4629886d0d1db8bd60d1dd2212823afb096c
Amadea Yinata <amadeayinata@gmail.com>
2024-05-18 06:59:24 +0800

    Front-end updates
    Changes were made to:
    - login.css
    -style.css
    -about-us.html
    -index.html
    -login.html
    -sign-up.html
    
    Complete documentation along with explanation will be uploaded to the pull request/ issues.

--- project/app/templates/login.js
+++
@@ -1,6 +0,0 @@
-// script.js
-document.querySelectorAll('.login-btn').forEach(button => {
-    button.addEventListener('click', () => {
-        alert('Login as ' + button.textContent.trim());
-    });
-});
