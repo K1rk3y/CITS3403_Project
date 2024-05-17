1532f80163e603e1a3dbda21748b5ff088f3bced
K1rk3y <133004197+K1rk3y@users.noreply.github.com>
2024-05-17 14:12:26 +0800

    Added fixes for Logout function
    Fixed the links on home pages
    Added support for logout function

---
+++ project/app/form_class.py
@@ -0,0 +1,51 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, ValidationError, SelectField, IntegerField, TextAreaField
+from wtforms.validators import DataRequired, EqualTo, Length, Email
+from app.validators import phone_number_validator, address_validator, instructions_length_validator, number_of_people_validator
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
+    password = PasswordField('Password', validators=[
+        DataRequired(),
+        Length(min=6),
+        EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Confirm Password', validators=[DataRequired()])
+    role = SelectField('Role', choices=[('Homecook', 'Homecook'), ('Patron', 'Patron')], validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        from app.model import User  # Import here to avoid circular imports
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is already taken. Please choose a different one.')
+        
+        
+class OrderForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired(), address_validator])
+    suburb = StringField('Suburb', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    phone = StringField('Phone', validators=[DataRequired(), phone_number_validator])
+    meal = SelectField('Meal', choices=[
+        ('normal', 'Normal'), ('vegan', 'Vegan'),
+        ('vegetarian', 'Vegetarian'), ('pescatarian', 'Pescatarian'),
+        ('nut-free', 'Nut-Free')
+    ], validators=[DataRequired()])
+    number_of_people = IntegerField('Number of People', validators=[DataRequired(), number_of_people_validator])
+    instructions = TextAreaField('Special Instructions', validators=[instructions_length_validator])
+    submit = SubmitField('Submit')
+
+
+class SearchForm(FlaskForm):
+    query = StringField('Search Orders', validators=[DataRequired()])
+    submit = SubmitField('Search')
