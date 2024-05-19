9e0874892fd5c64177b6805d0719f598b0539295
K1rk3y <133004197+K1rk3y@users.noreply.github.com>
2024-05-19 16:46:45 +0800

    Delete project directory

--- project/app/submit/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, session, request
-
-submit_bp = Blueprint('submit', __name__)
-
-@submit_bp.route('/submit')
-def dashboard():
-    return 'submit page'
